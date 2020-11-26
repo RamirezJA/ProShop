cdd80a1ea892b4a71d5309b6a124f02e60f2b0b7
RamirezJA <26813102+RamirezJA@users.noreply.github.com>
2020-11-26 00:01:18 -0800

    test

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import colors from 'colors'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
-//m11
+//m12
 dotenv.config()
 
 connectDB()
@@ -12,35 +12,38 @@ connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('API is running...')
+  res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
 
 app.use((req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`)
-    res.status(404)
-    next(error)
+  const error = new Error(`Not Found - ${req.originalUrl}`)
+  res.status(404)
+  next(error)
 })
 
 app.use((err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-    res.status(statusCode)
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-    })
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  res.status(statusCode)
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  })
 })
 
 app.get('/api/products', (req, res) => {
-    res.json(products)
+  res.json(products)
 })
 
 app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
+  const product = products.find((p) => p._id === req.params.id)
+  res.json(product)
 })
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
+app.listen(
+  PORT,
+  console.log(`Server ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
+)
