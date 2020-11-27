a15ab28731730fd048c7fc73a6b129d96f162056
RamirezJA <26813102+RamirezJA@users.noreply.github.com>
2020-11-27 11:18:43 -0800

    product reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { red } from 'colors'
+import { productListReducers } from './reducers/productReducers.js'
 
 const reducer = combineReducers({})
 
